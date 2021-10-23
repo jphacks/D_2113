182d1edc4916afe6354df4a62f11eafa09ca6590
Neru-neru <yumadachi0113soccer@gmail.com>
2021-10-23 13:42:21 +0900

    made taking photo by camera and selecting photo in library.

--- cloud_app/lib/result.dart
+++ cloud_app/lib/result.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:async';
+import 'dart:io';
 
 class Result extends StatelessWidget {
+  Result(this._image);
+  File? _image;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cloud Recognition'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              child: Text('結果'),
-            )
-          ]
+        appBar: AppBar(
+          title: const Text('Cloud Recognition'),
         ),
-      )
-    );
+        body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  child: const Text('結果'),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(20),
+                  width: 300,
+                  child: Image.file(_image!)
+                ),
+              ]),
+        ));
   }
 }
--- cloud_app/lib/takingPhoto.dart
+++ cloud_app/lib/takingPhoto.dart
@@ -1,40 +1,95 @@
+import 'package:cloud_app/main.dart';
 import 'package:cloud_app/result.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:async';
+import 'dart:io';
 
-class TakingPhoto extends StatelessWidget {
+class TakingPhoto extends StatefulWidget {
+  @override
+  _TakingPhoto createState() => _TakingPhoto();
+}
+
+class _TakingPhoto extends State<TakingPhoto> {
+  File? _image;
+  final picker = ImagePicker();
+
+  Future getImageFromCamera() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      }
+    });
+  }
+
+  Future getImageFromGallery() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cloud Recognition'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.all(10),
-              child: const Text("雲を撮ろう！"),
-            ),
-            Container(
-              margin: const EdgeInsets.all(10),
-              child: ElevatedButton.icon(
-                label: const Text('結果を見る'),
-                icon: const Icon(Icons.cloud),
-                onPressed: () => {
-                  Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) {
-                      return Result();
-                    }))
-                },
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(240, 50)
+        appBar: AppBar(
+          title: const Text('Cloud Recognition'),
+        ),
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              Center(
+                child: Container(
+                  margin: const EdgeInsets.all(20),
+                  width: 300,
+                  child:
+                    Center(
+                      child: _image == null
+                      ? const Text("雲を撮ろう！")
+                      : Image.file(_image!)
+                    ),
                 ),
               ),
-            ),
-          ]
-        )
-      )
-    );
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FloatingActionButton(
+                    heroTag: "hero1",
+                    onPressed: getImageFromCamera, //カメラから画像を取得
+                    tooltip: 'Pick Image From Camera',
+                    child: Icon(Icons.add_a_photo),
+                  ),
+                  FloatingActionButton(
+                    heroTag: "hero2",
+                    onPressed: getImageFromGallery, //ギャラリーから画像を取得
+                    tooltip: 'Pick Image From Gallery',
+                    child: Icon(Icons.photo_library),
+                  ),
+                ],
+              ),
+              Container(
+                margin: const EdgeInsets.all(20),
+                child: _image == null
+                  ? const SizedBox.shrink()
+                  : ElevatedButton.icon(
+                      label: const Text('結果を見る'),
+                      icon: const Icon(Icons.cloud),
+                      onPressed: () => {
+                        Navigator.of(context)
+                            .push(MaterialPageRoute(builder: (context) {
+                          return Result(_image);
+                        }))
+                      },
+                      style:
+                          ElevatedButton.styleFrom(fixedSize: const Size(240, 50)),
+                    ),
+              ),
+            ])));
   }
 }
